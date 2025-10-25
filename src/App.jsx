54df716a27cf378f591872bd854d3195a02ed83c
lucaolv <111596218+lucaolv@users.noreply.github.com>
2025-10-24 21:51:45 -0300

    navegacao com os cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Footer from './components/Footer/Footer'
 import './App.css'
@@ -7,7 +7,13 @@ function App() {
   return (
     <div className="app-layout">
       <div className="app-container">
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/hero/:heroId" element={<div>Página do Herói (Placeholder)</div>} />
+
+          <Route path="*" element={<div>Página não encontrada</div>} />
+        </Routes>
       </div>
       <Footer />
     </div>
